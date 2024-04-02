5001bd880b0d74fce32b78ad40b4e5fc14f198f7
MRJAHID <mrjahid350@gmail.com>
2024-04-03 01:46:47 +0600

    Task load on Task List

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,8 +1,19 @@
 import SearchTask from "./SearchTask.jsx";
 import TaskActions from "./TaskActions.jsx";
 import TaskList from "./TaskList.jsx";
+import {useState} from "react";
 
 const TaskBoard = () => {
+    const defaultTask = {
+        'id': crypto.randomUUID(),
+        'title': "Learn React Native",
+        'description': "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+        'tags': ['Web', 'React', 'JS'],
+        'priority': 'High',
+        'isFavorite': true,
+    }
+    const [task, setTask]  = useState([defaultTask]);
+
     return (
         <section className="mb-20" id="tasks">
 
@@ -14,7 +25,7 @@ const TaskBoard = () => {
                 <div
                     className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                     <TaskActions/>
-                    <TaskList/>
+                    <TaskList task={task}/>
                 </div>
             </div>
         </section>

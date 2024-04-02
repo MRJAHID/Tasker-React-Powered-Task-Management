9c4ed61ec2bff908fe466e00aa763307d62993ad
MRJAHID <mrjahid350@gmail.com>
2024-04-03 02:02:26 +0600

    Modal show

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -2,6 +2,7 @@ import SearchTask from "./SearchTask.jsx";
 import TaskActions from "./TaskActions.jsx";
 import TaskList from "./TaskList.jsx";
 import {useState} from "react";
+import AddTaskModal from "./AddTaskModal.jsx";
 
 const TaskBoard = () => {
     const defaultTask = {
@@ -12,10 +13,15 @@ const TaskBoard = () => {
         'priority': 'High',
         'isFavorite': true,
     }
-    const [task, setTask]  = useState([defaultTask]);
+    const [task, setTask] = useState([defaultTask]);
+    const [showAddModal, setShowAddModal] = useState(false);
+
 
     return (
         <section className="mb-20" id="tasks">
+
+            {/* Show Modal */}
+            {showAddModal && <AddTaskModal/>}
 
             <div className="container">
                 <div className="p-2 flex justify-end">
@@ -24,7 +30,7 @@ const TaskBoard = () => {
 
                 <div
                     className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-                    <TaskActions/>
+                    <TaskActions onAddTask={() => setShowAddModal(true)}/>
                     <TaskList task={task}/>
                 </div>
             </div>

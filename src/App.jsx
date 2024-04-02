3c8e7f7f4a71a4e9c106d0f98f5a545898c166ae
MRJAHID <mrjahid350@gmail.com>
2024-04-03 01:27:36 +0600

    TaskBoard Design

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import TaskBoard from "./components/Task/TaskBoard.jsx";
 
 function App() {
 
     return (
         <>
             <Header/>
-            <Hero/>
+            <div className='flex flex-col justify-center items-center'>
+                <Hero/>
+                <TaskBoard/>
+            </div>
             <Footer/>
         </>
     )
